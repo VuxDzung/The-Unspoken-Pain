f7d198528efc0b76915dc08d9b5fc62da168afcc
Le Hong Phuc <phucme241@gmail.com>
2023-10-04 09:55:29 +0700

    update change Canvas method

---
+++ ssg104-project/Assets/SSG/Scripts/Manager/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    PlayerController player => FindObjectOfType<PlayerController>();
+    [SerializeField] private GameObject[] activeObject;
+    [SerializeField] private Canvas[] gameCanvas;
+    void Start()
+    {
+        
+    }
+    private void setObjInGame(int ord)
+    {
+        if (ord != 0) 
+            foreach (var obj in activeObject)  obj.SetActive(false); 
+        else
+            foreach (var obj in activeObject)  obj.SetActive(true); 
+    }
+    // 0: default
+    // array order + 1: canvas
+    public void ChangeCanvas(int ord)
+    {
+        setObjInGame(ord);
+        foreach (Canvas c in gameCanvas)
+            if (gameCanvas[ord] == c) c.gameObject.SetActive(true);
+            else if (gameCanvas[ord].enabled) c.gameObject.SetActive(false);
+    }
+    void Update()
+    {
+        
+    }
+}
