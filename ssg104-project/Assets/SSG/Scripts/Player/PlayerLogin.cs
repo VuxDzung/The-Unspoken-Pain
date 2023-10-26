8a69699fa088ee01e3236d603c5e00a32f64eae9
Vũ Đức Dũng <vuducdung03042004@gmail.com>
2023-10-26 23:13:31 +0700

    add login field

--- ssg104-project/Assets/SSG/Scripts/Player/PlayerLogin.cs
+++ ssg104-project/Assets/SSG/Scripts/Player/PlayerLogin.cs
@@ -13,9 +13,16 @@ public class PlayerLogin : MonoBehaviour
     private static string newGameScene = "OpeningScene";
     [SerializeField] private TMP_InputField tmpPlayerNameInput;
     [SerializeField] private Button confirmBtn;
+    [SerializeField] private TextMeshProUGUI warningTxt;
+    [SerializeField] private TextMeshProUGUI warning_HasData; 
 
     private void Start()
     {
+        warningTxt.gameObject.SetActive(false);
+
+        if (SaveLoadSystem.HasPlayerData()) warning_HasData.gameObject.SetActive(true);
+        else warning_HasData.gameObject.SetActive(false);
+
         if (confirmBtn != null)
             confirmBtn.onClick.AddListener(SetNewGameData);
     }
@@ -23,14 +30,28 @@ public class PlayerLogin : MonoBehaviour
 
     public void SetNewGameData()
     {
+        SaveLoadSystem.SaveScene("");
+        SaveLoadSystem.SavePlatformer(null);
+        SaveLoadSystem.SaveStory(null);
+
         string name = tmpPlayerNameInput.text;
         if (name.Length == 0)
         {
             Debug.LogError("Player name input field cannot be empty");
+            warningTxt.gameObject.SetActive(true);
+            warningTxt.text = "Player name input field cannot be empty";
+            return;
+        }
+        else if (name.Length > 13)
+        {
+            Debug.LogError("Player name cannot longer than 13 digits");
+            warningTxt.gameObject.SetActive(true);
+            warningTxt.text = "Player name cannot longer than 13 digits";
+            return;
         }
         else
         {
-            SaveLoadSystem.Save(name);
+            SaveLoadSystem.SavePlayerName(name);
             GameManager.Instance.ChangeToScene(newGameScene);
         }
     }
