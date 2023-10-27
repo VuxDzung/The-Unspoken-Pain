f42588d17403551eb2629e493cb3d1894777e74c
Kienlabadao <nguyentrungkien863@gmail.com>
2023-10-27 13:27:34 +0700

    Finish interactable link in platformer scenes
    Add dialogues and link interactable

--- ssg104-project/Assets/SSG/Scripts/MVC/Objects/Mechanism/TriggeringItems.cs
+++ ssg104-project/Assets/SSG/Scripts/MVC/Objects/Mechanism/TriggeringItems.cs
@@ -17,7 +17,10 @@ public class TriggeringItems : MonoBehaviour
     void Trigger(GameObject obj)
     { 
         if (triggedInteractiveObjects.Length > 0)
+        {
             GameManager.Instance.AddInteractedItemsByName(triggedInteractiveObjects);
+            Debug.LogError("Add object");
+        }
 
         if (triggedInventoryItems.Length > 0)
             GameManager.Instance.AddInventoryItemByName(triggedInventoryItems);
